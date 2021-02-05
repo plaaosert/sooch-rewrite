026dc30161e407671a93cb6c49dfd54aa7e81a06
Chan Wen Xu <lutherchanpublic@gmail.com>
2021-02-05 13:42:49 +0800

    message: Handle aliases and upper case correctly

--- sooch/commands/help.py
+++ sooch/commands/help.py
@@ -1,16 +1,20 @@
+"""
+Module that contains command that provide information on how to progress to
+the player.
+"""
 from typing import Optional
 
 import discord
-import sooch.message as msg
 
 # Preload all help embeds
 help_embeds: dict[str, discord.Embed] = {}
 
 
-def populate_help_embeds(commands: dict[str, any]):
+def populate_help_embeds(commands: list[any]):
+    """Populate the help embed cache with the provided list of commands."""
     default_text = ""
 
-    for command_name, command in commands.items():
+    for command in commands:
         # Create joined aliases for help embeds and the default embed
         aliases = ' '.join(command.aliases) or "None"
 
@@ -20,23 +24,27 @@ def populate_help_embeds(commands: dict[str, any]):
                                                aliases,
                                                command.syntax)
 
-        help_embeds[command_name] = discord.Embed().add_field(
-            name="Help for `{}`".format(command_name),
+        help_embeds[command.name] = discord.Embed().add_field(
+            name="Help for `{}`".format(command.name),
             value=embed_text,
             inline=True
         )
 
         # This type of help text works for now
         # Once all features have been implemented, we can rework the help embed
-        default_text += "`{}` *or* `{}`\n".format(command_name, aliases)
+        default_text += "`{}` *or* `{}`\n".format(command.name, aliases)
 
     # Set the default text after setting all the help texts
-    help_embeds["default"] = discord.Embed().add_field(name="Help", value=default_text, inline=True)
+    help_embeds["default"] = discord.Embed().add_field(
+        name="Help", value=default_text, inline=True)
 
 
 async def help_command(client: discord.Client,
                        message: discord.Message,
                        content: list[str]) -> Optional[discord.Embed]:
+    """Handle the s!help command."""
+    del client, message
+
     # Initialize the embed + parsed arguments
     embed = discord.Embed()
     help_command_arguments = None
@@ -47,9 +55,11 @@ async def help_command(client: discord.Client,
 
     # Adds "s!" to the beginning if it is not there
     else:
-        help_command_arguments = content[1] if content[1].startswith("s!") else "s!" + content[1]
+        help_command_arguments = content[1] if content[1].startswith(
+            "s!") else "s!" + content[1]
 
-    # Otherwise check if the argument is valid and that there is only one argument
+    # Otherwise check if the argument is valid and that there is only one
+    # argument
     if help_command_arguments in help_embeds.keys() and not content[2:]:
         embed = help_embeds.get(help_command_arguments)
 
@@ -58,12 +68,14 @@ async def help_command(client: discord.Client,
         embed.add_field(
             name="Invalid Arguments",
             value="`{}` is an invalid command \n\n"
-                  "Correct syntax: `s!help <command>`".format(' '.join(content[1:])),
+                  "Correct syntax: `s!help <command>`".format(
+                      ' '.join(content[1:])),
             inline=True
         )
 
         embed.set_footer(
-            text="If you think this is an error, please contact the devs or submit a bug report with s!report"
+            text=("If you think this is an error, please contact the devs or"
+                  "submit a bug report with s!report")
         )
 
     return embed
